0c7196a231facc3ecbc8434bb9327f349e1fbf86
lasha bedenashvili <Lasha.bedenashvili1@gmail.com>
2023-03-27 15:27:46 +0400

    createUserTableDataBase

---
+++ VehicleStateCodes.Data.Domein/Migrations/20230327112008_initialUser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleStateCodes.Data.Domein.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SurName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: true),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Cityzen = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPasswordHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "date", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPasswordHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPasswordHistory_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPasswordHistory_UserId",
+                table: "UserPasswordHistory",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPasswordHistory");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
--- VehicleStateCodes/Program.cs
+++ VehicleStateCodes/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using NLog.Web;
 using VehicleStateCodes;
+using VehicleStateCodes.Data.Domein.Domein;
+using VehicleStateCodes.DataBase.UnitOfWork;
 
 var logger = NLog.LogManager
     .Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
@@ -17,10 +20,13 @@ try
     builder.Host.UseNLog();
 
     // Add services to the container.
+    builder.Services.AddDbContext<Context>(options => options
+    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddScoped<IUnitOfWork, UnitOFWork>();
     builder.Services.AddSwaggerGen();
 
     var app = builder.Build();
